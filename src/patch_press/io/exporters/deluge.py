7dd1dfc36b345262041f8db05dd9a2c5f42e97ee
Aurélien Leblond <blablack@gmail.com>
2026-05-12 23:19:00 +0100

    OSC level at 30

--- src/patch_press/io/exporters/deluge.py
+++ src/patch_press/io/exporters/deluge.py
@@ -133,7 +133,7 @@ class DelugeExporter:
         dp = etree.SubElement(
             sound, "defaultParams",
             arpeggiatorGate="0x00000000", portamento="0x80000000",
-            compressorShape="0xDC28F5B2", oscAVolume="0x4CCCCCC4",
+            compressorShape="0xDC28F5B2", oscAVolume="0x19999999",
             oscAPulseWidth="0x00000000", oscBVolume="0x80000000",
             oscBPulseWidth="0x00000000", noiseVolume="0x80000000",
             volume="0x4CCCCCA8", pan="0x00000000",
